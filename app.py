4516131e3bf485b5ff3321cd515c785af7f069c8
Faria Akter Ride <ridef90@gmail.com>
2026-08-18 18:21:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ st.markdown("""
 @st.cache_resource
 def load_llm():
     return ChatGroq(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-120be",
         temperature=0.2,
         groq_api_key=st.secrets["GROQ_API_KEY"],
         model_kwargs={"response_format": {"type": "json_object"}}
